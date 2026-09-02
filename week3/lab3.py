13955c568fdd62c6afa2142220b726090a9a73a8
fgrodriguez23 <fgrodriguez@csi.edu>
2026-09-02 15:34:08 -0600

    added week 3

--- week3/lab3.py
+++ week3/lab3.py
@@ -1,1 +1,30 @@
-#this is for lab 3
+number1 = 5
+number2= 2.5
+print (number1 + number2)
+#yes you can use them using
+
+number1 = int(input("Enter a whole number: "))
+number2= float(input("Enter a decimal number: "))
+print(number1 + number2)
+#a string is text
+
+name= "francisco"
+sentence = "my name is "+ name 
+print(sentence)
+print (name[0])
+print(name[0:3])
+# the list can have diferent types of data 
+
+mylist = ["apple", "pears", "watermelon", "mango"]
+print(mylist )
+mylist.append("tangerine")
+print(mylist) 
+mylist.remove("mango")
+print (len(mylist))
+# it add things to the list 
+
+
+
+#i leard intergers are whole numbers and floats are decimal numbers
+#i leard how to remove and add things to a list and how to get the length of a list
+#i leard that strings are use for text
