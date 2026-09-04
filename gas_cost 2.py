205e088c5bb09fa96d171f01bbff177392701cb8
fgrodriguez23 <fgrodriguez@csi.edu>
2026-09-04 17:57:24 -0600

    Add files via upload

---
+++ gas_cost 2.py
@@ -0,0 +1,12 @@
+# Francisco Rodriguez
+# This program calculates how much gas costs.
+
+# These variables store the gallons and price.
+gallons_of_gas = 4
+price_per_gallon = 3
+
+# Multiply the variables to find the total cost.
+total_cost = gallons_of_gas * price_per_gallon
+
+# Print the answer.
+print("The gas costs", total_cost, "dollars.")
