27d0637baa474d2d1676c230795f27b959efd669
francisco rodriguez <fgrodriguez@csi.edu>
2026-09-10 13:03:34 -0500

    debug-1fix.py

--- project 1/debug-1.py
+++ project 1/debug-1.py
@@ -3,11 +3,17 @@
 
 # This program should add three numbers together.
 
-total = 0
-num1 = input("What's the first number? >")
-total = num1
-num2 = input("What's the second number? >")
-total = num2
-num3 = input("What's the third number? >")
-total = num3
+
+num1 = int(input("What's the first number? >"))
+num2 = int(input("What's the second number? >"))
+num3 = int(input("What's the third number? >"))
+
+total = num1 + num2 + num3
+
 print(f"Total is: {total}")
+
+
+
+
+
+
